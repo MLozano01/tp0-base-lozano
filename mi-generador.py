2d1c6cedc5ac339ee472afc7fd24f6c15eaec86d
Martu <mlozano@fi.uba.ar>
2025-03-17 17:51:23 -0300

    try adding volumes fixing? path

--- mi-generador.py
+++ mi-generador.py
@@ -43,7 +43,7 @@ def create_client(id):
     depends_on:
       - server\n
     volumes:
-      - client_config:/client/config.yaml
+      - ./client:/client/config.yaml
     """ 
     return client
 
