7d35dd2569c8721e64d65331d9998456bb125add
Martu <mlozano@fi.uba.ar>
2025-03-21 15:42:07 -0300

    nuevo netcat

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,17 +2,21 @@
 
 docker run -dit --network=tp0_testing_net --name=server_tester ubuntu sh
 
-docker exec -it server_tester sh -c "apt update && apt install -y  netcat-traditional"
+docker exec -it server_tester sh -c "apt update && apt install -y  netcat-openbsd"
 
 TEST_MESSAGE="Potter"
 
 PORT="12345"
 
-SERVER_ANS=$(docker exec -it server_tester sh -c "echo $TEST_MESSAGE | nc server $PORT | tr -d '\r'")
+SERVER_ANS=$(docker exec -it server_tester sh -c "echo $TEST_MESSAGE | nc server $PORT | tr -d '\r\n'")
 
-echo $SERVER_ANS
+# echo $SERVER_ANS
 
-if [[ $SERVER_ANS = $TEST_MESSAGE ]]
+# n=$(expr length "$SERVER_ANS")
+# echo "Length of the string is : $n"
+
+
+if [[ "$SERVER_ANS" = "$TEST_MESSAGE" ]]
 then 
     echo "action: test_echo_server | result: success"
 else 
