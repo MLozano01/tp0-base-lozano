471ff9f128bc1f64d7284a7a54a4b1a0579dc3a2
Martu <mlozano@fi.uba.ar>
2025-03-22 13:05:08 -0300

    limpiando el bash

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -10,15 +10,6 @@ C_NAME="server_tester"
 
 SERVER_ANS=$(docker run --rm --network=$NET --name=$C_NAME busybox sh -c "echo $TEST_MESSAGE | nc -w 5 server $PORT")
 
-# docker exec server_tester sh -c "apt update && apt install -y  netcat-openbsd"
-
-# SERVER_ANS=$(docker exec server_tester sh -c "echo $TEST_MESSAGE | nc -w 5 server $PORT | tr -d '\r\n'")
-
-# echo $SERVER_ANS
-
-# n=$(expr length "$SERVER_ANS")
-# echo "Length of the string is : $n"
-
 if [ "$SERVER_ANS" = "$TEST_MESSAGE" ]
 then 
     echo "action: test_echo_server | result: success"
